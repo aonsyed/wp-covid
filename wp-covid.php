19ac6bbf4cc36feabf2d09d39f0015dead96cb56
Muttahir Aon Syed <aon.syed14@gmail.com>
2020-03-31 11:14:29 +0500

    Create wp-covid.php

---
+++ wp-covid.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: WP-Covid
+Description: Campaign to raise awareness about the corona virus by redirectly to official resources from the user's country
+Tags: COVID-19, Covid-19, Corona Virus, Coronavirus, Corona, Virus, Wordpress, Covid
+Author: Aon Syed
+Author URI: 
+Version: 1.0.0
+Text Domain: wp-covid
+Domain Path: /languages/
+Requires PHP: 5.2
+License: GPL v3 or later
+*/
